fb1b871e2dceda130c719d5d2e83f624973ad151
balajiofficial06 <106816231+balajiofficial06@users.noreply.github.com>
2023-10-14 17:22:50 +0000

    intial setUp

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { useState } from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
